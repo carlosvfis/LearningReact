5de7c70973c891280fe783178121d9766df4b72d
Carlos Matheus <carlos.matheus@vfis.tdtu.edu.vn>
2023-01-30 07:29:29 +0000

    Update App.js and Testimonio.js changes

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,22 +1,14 @@
-import React from 'react';
-import '../hojas-de-estilo/Testimonio.css';
+import React from "react";
+import "../hojas-de-estilo/Testimonio.css";
 
-function Testimonio(props) {
+function Testimonio() {
   return (
-    <div className='contenedor-testimonio'>
-      <img 
-        className='imagen-testimonio'
-        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
-        alt='Foto de Emma'/>
-      <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>
-          <strong>{props.nombre}</strong> en {props.pais}
-        </p>
-        <p className='cargo-testimonio'>
-          {props.cargo} en <strong>{props.empresa}</strong>
-        </p>
-        <p className='texto-testimonio'>"{props.testimonio}"</p>
-      </div>
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require("../imagenes/testimonio-emma.png")}
+        alt=""
+      />
     </div>
   );
 }
