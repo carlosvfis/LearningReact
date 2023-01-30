8dd12e26bf03511d399b85069c4052e2e75f2432
Carlos Matheus <carlos.matheus@vfis.tdtu.edu.vn>
2023-01-30 08:53:50 +0000

    Update 3 files. Terminado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
-import Testimonio from './componentes/Testimonio';
+import "./App.css";
+import Testimonio from "./componentes/Testimonio";
 
 function App() {
   return (
-    <div className='App'>
-      
+    <div className="App">
+      <div className="contenedor-principal">
+        <h1>Esto es lo que dicen nuestros estudiantes</h1>
+        <Testimonio 
+          nombre='Minerva Perez'
+          pais='Suecia'
+          imagen='emma'
+          cargo='Ingeniera en Computacion'
+          empresa='PDVSA'
+          testimonio='Siento que debo buscar otro trabajo'
+          />
+         
+        <Testimonio
+          nombre='Carlos'
+          pais='Venezuela'
+          imagen='shawn'
+          cargo='Tecnico Superior'
+          empresa='PDVSA'
+          testimonio='Siento que debo buscar otro trabajo'
+          
+        />
+      </div>
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "../hojas-de-estilo/Testimonio.css";
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
       <img
         className="imagen-testimonio"
-        src={require("../imagenes/testimonio-emma.png")}
-        alt=""
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt="{props.nombre}"
       />
-    </div>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+        <p className="texto-testimonio">{props.testimonio}</p> 
+      </div>
+    </div> // end of main contenedor
   );
 }
 
